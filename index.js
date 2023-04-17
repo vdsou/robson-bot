91f6d6d7da041a341042cd8179d95015e35b58ee
Valdir <vdsouza@outlook.com>
2023-04-17 18:29:38 -0300

    add event listening and update API URL

--- index.js
+++ index.js
@@ -6,21 +6,38 @@ const apiConnect = require("./services/apiConnectTest");
 dotenv.config();
 
 const TOKEN = process.env.DISCORD_TOKEN;
+const TEXT_JOKE = process.env.TEXT_JOKE;
+const USER_JOKE = process.env.USER_JOKE;
+const IMAGE_JOKE = process.env.IMAGE_JOKE;
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageTyping,
     GatewayIntentBits.GuildVoiceStates,
   ],
 });
+
+console.log(TEXT_JOKE, USER_JOKE);
 client.on("messageCreate", commandHandler);
 client.on("messageCreate", (message) => {
   if (message.content === "oi") {
     message.reply("Oi!!!");
   }
 });
-
+client.on("typingStart", (interaction) => {
+  if (interaction.user.id === USER_JOKE) {
+    const channel = interaction.channel;
+    channel
+      .send({ content: `${TEXT_JOKE}` })
+      .then((message) => {
+        setTimeout(() => message.delete(), 5000);
+      })
+      .catch();
+  }
+});
 client.once(Events.ClientReady, async (client) => {
   const api = await apiConnect;
   console.log(api);
--- services/api.js
+++ services/api.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
 
+const API_URL = process.env.API;
 const api = axios.create({
-  baseURL: "http://localhost:3000/app/",
+  baseURL: API_URL,
 });
 module.exports = api;
