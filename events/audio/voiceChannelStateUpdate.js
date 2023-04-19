ad28a7dc8725f8f1b47b5bdbfda4ad1f7e0bb312
Valdir <vdsouza@outlook.com>
2023-04-19 19:23:00 -0300

    Revert "update voiceChannelStateUpdate identetion and make it to ignore user mic mute/unmute state. so it doesn't play"
    This reverts commit a47eb9121ea60ac931b225e8f677ac221b661c75.

--- events/audio/voiceChannelStateUpdate.js
+++ events/audio/voiceChannelStateUpdate.js
@@ -1,4 +1,4 @@
-const { channelIds } = require("../../config/channelsId");
+const {channelIds} = require("../../config/channelsId")
 const playJokeAudio = require("./playJokeWhenOneEnters");
 // must have config channel ids file
 module.exports = async (oldState, newState) => {
@@ -6,41 +6,22 @@ module.exports = async (oldState, newState) => {
   const newInChannel = newState.channel;
   const oldInChannel = oldState.channel;
 
-  if (user?.bot) {
-    return;
+  if(user?.bot){
+    return
   }
-  if (oldState.mute !== newState.mute) {
-    return;
-  }
-  console.log(channelIds);
-  if (
-    oldInChannel === null &&
-    newInChannel !== null &&
-    channelIds.includes(newInChannel.id)
-  ) {
-    console.log("user joined the channel!");
+  console.log(channelIds)
+  if (oldInChannel === null && newInChannel !== null && channelIds.includes(newInChannel.id)) {
+    console.log("user joined the channel!")
     playJokeAudio(newState);
   }
-  if (
-    oldInChannel !== null &&
-    newInChannel !== null &&
-    channelIds.includes(newInChannel.id)
-  ) {
-    console.log("user moved to this channel!");
+  if(oldInChannel !== null && newInChannel !== null && channelIds.includes(newInChannel.id)){
+    console.log("user moved to this channel!")
     playJokeAudio(newState);
   }
-  if (
-    oldInChannel !== null &&
-    newInChannel === null &&
-    channelIds.includes(oldInChannel.id)
-  ) {
-    console.log("user left this channel!");
+  if(oldInChannel !== null && newInChannel === null && channelIds.includes(oldInChannel.id)){
+    console.log("user left this channel!")
   }
-  if (
-    oldInChannel !== null &&
-    newInChannel !== null &&
-    channelIds.includes(oldInChannel.id)
-  ) {
-    console.log("user moved to another channel!");
+  if(oldInChannel !== null && newInChannel !== null && channelIds.includes(oldInChannel.id)){
+    console.log("user moved to another channel!")
   }
 };
